066a4d5c8b590e292df1454add0edb2e102a2cec
CKline22 <CKline.1022@gmail.com>
2022-04-02 14:34:26 -0400

    restarted on part four

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-const express = require("express");
+// DEPENDENCIES
+const express = require('express')
 
 // CONFIGURATION
-require("dotenv").config();
-const PORT = process.env.PORT;
-const app = express();
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
 
 // MIDDLEWARE
-app.set("views", __dirname + "/views");
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "jsx");
-app.engine("jsx", require("express-react-views").createEngine());
-app.use(express.static("public"));
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
-//routes
-app.get("/", (req, res) => {
-  res.send("Welcome to an answome app about breads");
-});
+app.use(express.static('public'))
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an Awesome App about Breads')
+})
+
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
 // 404 Page
-app.get("*", (req, res) => {
-  res.send("404");
-});
+app.get('*', (req, res) => {
+  res.send('404')
+})
 
-//breads
-const breadsController = require(".controllers/breads_controller.js");
-app.use("/breads", breadsController);
-
-//listen
+// LISTEN
 app.listen(PORT, () => {
-  console.log("nomming at port", PORT);
-});
+  console.log('nomming at port', PORT);
+})
--- views/index.jsx
+++ views/index.jsx
@@ -1,27 +1,26 @@
-const React = require("react");
-const Default = require("./layouts/Default");
+const React = require('react')
+const Default = require('./layouts/Default')
 
-function Index({ breads }) {
+function Index ({breads})  {
   return (
     <Default>
       <h2>Index Page</h2>
-      {/*<p>I have {breads[0].name} bread</p>*/}
+      {/*<p>I have {breads[0].name} bread!</p>*/}
       <ul>
-        {breads.map((bread, index) => {
-          return (
+        {
+          breads.map((bread, index)=> {
+            return (
             <li key={index}>
-              <a href={`/breads/${index}`}>{bread.name}</a>
+              <a href={`/breads/${index}`}>
+                {bread.name}
+              </a>
             </li>
-          );
-        })}
+            )
+          })
+        }
       </ul>
-      <div className="newButton">
-        <a href="/breads/new">
-          <button>Add a new bread</button>
-        </a>
-      </div>
     </Default>
-  );
+  )
 }
 
-module.exports = Index;
+module.exports = Index
